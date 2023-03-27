a4d3e20b718ce12dec1032534414217968e1a969
LeonidElkin <yolkinleon@mail.ru>
2023-03-27 03:17:49 +0300

    fix: Codestyle

--- BSTrees/src/main/kotlin/balancers/BSBalancer.kt
+++ BSTrees/src/main/kotlin/balancers/BSBalancer.kt
@@ -3,7 +3,7 @@ package balancers
 import binarySearchTree.BSNode
 import binarySearchTree.BSTree
 
-class BSBalancer<K:Comparable<K>, V>(tree: BSTree<K, V>):Balancer<K, V, BSNode<K, V>, BSTree<K, V>>(tree){
+class BSBalancer<K : Comparable<K>, V>(tree: BSTree<K, V>) : Balancer<K, V, BSNode<K, V>, BSTree<K, V>>(tree) {
 
     override fun balance() {}
 
