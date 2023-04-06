3daf43cdf01953c650b6ac6eaa4201cf0ca31321
Kirill Shishin <shishin_ka@mail.ru>
2023-04-06 11:45:15 +0300

    fix: Changing Balancers constructors and a small fix in leftRotate, rightRotate methods

--- BSTrees/src/main/kotlin/balancers/Balancer.kt
+++ BSTrees/src/main/kotlin/balancers/Balancer.kt
@@ -4,26 +4,28 @@ import BTree
 import Node
 
 abstract class Balancer<K : Comparable<K>, V, NODE_TYPE : Node<K, V, NODE_TYPE>, TREE_TYPE : BTree<K, V, NODE_TYPE, TREE_TYPE>>(
-    protected val tree: TREE_TYPE
+    protected var tree: TREE_TYPE?
 ) {
     protected fun leftRotate() {
-        val temp = tree.getLeftTree()
-        tree.setLeftTree(temp?.getRightTree())
+        val temp = tree?.getLeftTree()
+        tree?.setLeftTree(temp?.getRightTree())
         temp?.setRightTree(tree)
-        tree.updateHeight()
+        tree?.updateHeight()
         temp?.updateHeight()
+        tree = temp
     }
 
     protected fun rightRotate() {
-        val temp = tree.getRightTree()
-        tree.setRightTree(temp?.getLeftTree())
+        val temp = tree?.getRightTree()
+        tree?.setRightTree(temp?.getLeftTree())
         temp?.setLeftTree(tree)
-        tree.updateHeight()
+        tree?.updateHeight()
         temp?.updateHeight()
+        tree = temp
     }
 
     protected fun balanceFactory(): Int {
-        return (tree.getRightTree()?.getRoot()?.getHeight() ?: 0) - (tree.getLeftTree()?.getRoot()?.getHeight() ?: 0)
+        return (tree?.getRightTree()?.getRoot()?.getHeight() ?: 0) - (tree?.getLeftTree()?.getRoot()?.getHeight() ?: 0)
     }
 
     abstract fun balance()
--- BSTrees/src/main/kotlin/balancers/RBBalancer.kt
+++ BSTrees/src/main/kotlin/balancers/RBBalancer.kt
@@ -1,7 +1,11 @@
 package balancers
 
-open class RBBalancer: Balancer() {
+import redBlackTree.RBNode
+import redBlackTree.RBTree
 
+open class RBBalancer<K : Comparable<K>, V>(tree: RBTree<K, V>?) : Balancer<K, V, RBNode<K, V>, RBTree<K, V>>(tree) {
+
+    //update parent
     override fun balance() {
         TODO("Not yet implemented")
     }
