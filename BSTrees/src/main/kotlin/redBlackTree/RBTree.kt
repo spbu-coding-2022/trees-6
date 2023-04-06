cad65296cc8b2cb14b5150a93f1ca8f358459247
Kirill Shishin <shishin_ka@mail.ru>
2023-04-07 00:30:38 +0300

    feat: Added implementation of the 'add' method in RBTree

--- BSTrees/src/main/kotlin/redBlackTree/RBTree.kt
+++ BSTrees/src/main/kotlin/redBlackTree/RBTree.kt
@@ -12,11 +12,42 @@ class RBTree<K : Comparable<K>, V> : BTree<K, V, RBNode<K, V>>() {
     }
 
     override fun add(node: RBNode<K, V>) {
-        TODO("Not yet implemented")
+        var root = this.getRoot()
+        if (root == null) {
+            this.setRoot(node)
+            return
+        }
+        while (true) {
+            root ?: error("IT IS IMPOSSIBLE!!!")
+            if (root.getKey() > node.getKey()) {
+                val leftNode = root.getLeftNode()
+                if (leftNode == null) {
+                    node.setNodeColor(RBNode.Color.RED)
+                    root.setLeftNode(node)
+                    root.getLeftNode()?.setParent(root)
+                    balancer.balance(root)
+                    break
+                } else {
+                    root = leftNode
+                    val newTree = RBTree<K, V>()
+                    newTree.setRoot(root)
+
+                }
+            } else {
+                val rightNode = root.getRightNode()
+                if (rightNode == null) {
+                    node.setNodeColor(RBNode.Color.RED)
+                    root.setRightNode(node)
+                    root.getRightNode()?.setParent(root)
+                    break
+                } else {
+                    root = rightNode
+                }
+            }
+        }
     }
 
     override fun delete(key: K) {
         TODO("Not yet implemented")
     }
-
 }
