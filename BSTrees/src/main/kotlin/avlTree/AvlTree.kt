369703f6ee196357f7d5dbf44f24a7426694c9ef
LeonidElkin <yolkinleon@mail.ru>
2023-04-11 02:23:43 +0300

    feat: insert function is implemented

--- BSTrees/src/main/kotlin/avlTree/AvlTree.kt
+++ BSTrees/src/main/kotlin/avlTree/AvlTree.kt
@@ -1,14 +1,33 @@
 package avlTree
 
 import BTree
+import balancers.AvlBalancer
 
 class AvlTree<K: Comparable<K>, V>: BTree<K, V, AvlNode<K, V>>() {
 
+    private val balancer = AvlBalancer<K, V>()
+
     override fun insert(key: K, value: V) {
-        TODO("Not yet implemented")
+        add(AvlNode(key, value))
     }
     override fun add(node: AvlNode<K, V>) {
-        TODO("Not yet implemented")
+        val temp = this.root
+        if (temp?.getKey() == node.getKey() || temp == null) this.root = node
+        else {
+            val subTree = AvlTree<K, V>()
+            if (node.getKey() < temp.getKey()) {
+                subTree.root = temp.getLeftNode()
+                subTree.add(node)
+                this.root?.setLeftNode(subTree.root)
+            }
+            if (node.getKey() > temp.getKey()) {
+                subTree.root = temp.getRightNode()
+                subTree.add(node)
+                this.root?.setRightNode(subTree.root)
+            }
+        }
+
+        this.root = this.root?.let { balancer.balance(it) }
     }
 
     override fun delete(key: K) {
