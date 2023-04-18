7d7b6475cc17b322f2c7042cb4d8c9cd81330b4b
LeonidElkin <yolkinleon@mail.ru>
2023-04-18 17:24:32 +0300

    feat: Added find and deleting tests for BSTree

--- BSTrees/src/test/kotlin/binarySearchTree/BSTreeTest.kt
+++ BSTrees/src/test/kotlin/binarySearchTree/BSTreeTest.kt
@@ -31,15 +31,15 @@ class BSTreeTest {
     }
 
     @Test
-    fun `adding a node`(){
+    fun `adding a node`() {
         tree.insert(keyValue[0].first, keyValue[0].second)
-        Assertions.assertTrue(treeChecker.checkBsTreeInvariants(tree.root)){"Error adding a node"}
+        Assertions.assertTrue(treeChecker.checkBsTreeInvariants(tree.root)) { "Error adding a node" }
     }
 
     @Test
-    fun `adding nodes`(){
+    fun `adding nodes`() {
         for (i in keyValue) tree.insert(i.first, i.second)
-        Assertions.assertTrue(treeChecker.checkBsTreeInvariants(tree.root)){"Error adding nodes"}
+        Assertions.assertTrue(treeChecker.checkBsTreeInvariants(tree.root)) { "Error adding nodes" }
     }
 
     @Test
@@ -48,5 +48,60 @@ class BSTreeTest {
             tree.insert(i.first, i.second)
         }
         Assertions.assertTrue(treeChecker.checkBsTreeInvariants(tree.root)) { "Error adding nodes" }
+    }
+
+    @Test
+    fun `find test`() {
+        keyValue.forEach { tree.insert(it.first, it.second) }
+
+        keyValue.forEach {
+            Assertions.assertEquals(it.second, tree.find(it.first)) { "Find test error" }
+        }
+    }
+
+    @Test
+    fun `adding nodes with equal key`() {
+        keyValue.forEach { tree.insert(it.first, it.second) }
+
+        tree.insert(keyValue[0].first, keyValue[0].second + 1)
+
+        Assertions.assertEquals(
+            keyValue[0].second + 1,
+            tree.find(keyValue[0].first)
+        ) { "Error when adding nodes with equal key" }
+    }
+
+    @Test
+    fun `deleting a node`() {
+        tree.insert(keyValue[0].first, keyValue[0].second)
+        tree.delete(keyValue[0].first)
+        assertAll("The tree must be balanced and must not contain a node after deletion",
+            { Assertions.assertNull(tree.find(keyValue[0].first)) },
+            { Assertions.assertTrue(treeChecker.checkBsTreeInvariants(tree.root)) }
+        )
+    }
+
+    @Test
+    fun `deleting nodes`() {
+        keyValue.forEach { tree.insert(it.first, it.second) }
+        keyValue.forEach {
+            tree.delete(it.first)
+            assertAll("The tree must be balanced and must not contain a node after deletion",
+                { Assertions.assertNull(tree.find(it.first)) },
+                { Assertions.assertTrue(treeChecker.checkBsTreeInvariants(tree.root)) }
+            )
+        }
+    }
+
+    @Test
+    fun `deleting a lot of nodes`() {
+        bigKeyValue.forEach { tree.insert(it.first, it.second) }
+        bigKeyValue.forEach {
+            tree.delete(it.first)
+            assertAll("The tree must be balanced and must not contain a node after deletion",
+                { Assertions.assertNull(tree.find(it.first)) },
+                { Assertions.assertTrue(treeChecker.checkBsTreeInvariants(tree.root)) }
+            )
+        }
     }
 }
