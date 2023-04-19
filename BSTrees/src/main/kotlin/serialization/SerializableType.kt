76dd979579b614b41d135920dcdb4c1f4902ac92
Kirill Shishin <shishin_ka@mail.ru>
2023-04-19 07:28:17 +0300

    feat: New implementation of tree storage in sql

---
+++ BSTrees/src/main/kotlin/serialization/SerializableType.kt
@@ -0,0 +1,20 @@
+package serialization
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+class SerializableTree(
+    val name: String,
+    val typeOfTree: String,
+    val root: SerializableNode?
+)
+
+@Serializable
+class SerializableNode(
+    val key: String,
+    val value: String,
+    val metadata: String,
+    val leftNode: SerializableNode? = null,
+    val rightNode: SerializableNode? = null,
+    val parent: SerializableNode? = null,
+)
