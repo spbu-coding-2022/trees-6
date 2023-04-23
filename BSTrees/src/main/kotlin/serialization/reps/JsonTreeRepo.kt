364b0d06dd511ca7617a47dd6c9e851c29ae556a
Kirill Shishin <shishin_ka@mail.ru>
2023-04-23 22:27:36 +0300

    feat: Added Json tree repository implementation

---
+++ BSTrees/src/main/kotlin/serialization/reps/JsonTreeRepo.kt
@@ -0,0 +1,74 @@
+package serialization.reps
+
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import serialization.SerializableTree
+import kotlinx.serialization.encodeToString
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+import kotlin.io.path.Path
+
+
+object JsonTreeRepo : DBTreeRepo {
+    private fun createDirPaths() {
+        File("JSONTreeRep").mkdir()
+        File(Path("JSONTreeRep", "BSTree").toUri()).mkdir()
+        File(Path("JSONTreeRep", "RBTree").toUri()).mkdir()
+        File(Path("JSONTreeRep", "AvlTree").toUri()).mkdir()
+    }
+
+    private fun getPathToFile(typeTree: String, treeName: String): String {
+        return Path("JSONTreeRep", typeTree, "${treeName}.json").toString()
+    }
+
+    override fun getTree(typeTree: String, treeName: String): SerializableTree? {
+        createDirPaths()
+
+        val filePath = getPathToFile(typeTree, treeName)
+        lateinit var file: FileReader
+
+        return try {
+            file = FileReader(filePath)
+            val jsonText = file.readText()
+
+            Json.decodeFromString<SerializableTree>(jsonText)
+        } catch (e: NoSuchFileException) {
+            null
+        } catch (e: Exception) {
+            throw e
+        } finally {
+            file.close()
+        }
+    }
+
+    override fun setTree(serializableTree: SerializableTree) {
+        createDirPaths()
+
+        val filePath = getPathToFile(serializableTree.typeOfTree, serializableTree.name)
+        lateinit var file: FileWriter
+
+        try {
+            file = FileWriter(filePath)
+            file.write(Json.encodeToString(serializableTree))
+
+        } catch (e: Exception) {
+            throw e
+        } finally {
+            file.flush()
+            file.close()
+        }
+    }
+
+    override fun deleteTree(serializableTree: SerializableTree) {
+        createDirPaths()
+
+        val path = getPathToFile(serializableTree.typeOfTree, serializableTree.name)
+
+        try {
+            File(path).delete()
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+}
