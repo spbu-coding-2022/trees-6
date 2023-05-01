20c10465ff0366a89154c47d9bde261e655bacb9
Kirill Shishin <shishin_ka@mail.ru>
2023-05-01 07:19:14 +0300

    feat: Added the ability to add and delete a node from SerializableTree

---
+++ lib/src/main/kotlin/bstrees/model/dataBases/serialize/strategies/SerializeMethods.kt
@@ -0,0 +1,7 @@
+package bstrees.model.dataBases.serialize.strategies
+
+fun intToString(a: Int): String = a.toString()
+
+fun stringToInt(a: String): Int = a.toInt()
+
+fun stringToString(a: String): String = a
