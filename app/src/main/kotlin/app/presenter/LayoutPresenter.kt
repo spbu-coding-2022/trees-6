638a24b1754540326abd7a678ca0ac9bf617f29b
Kirill Shishin <shishin_ka@mail.ru>
2023-05-02 01:41:51 +0300

    feat: Added LayoutPresenter implementation

--- app/src/main/kotlin/app/presenter/LayoutPresenter.kt
+++ app/src/main/kotlin/app/presenter/LayoutPresenter.kt
@@ -2,25 +2,43 @@ package app.presenter
 
 import bstrees.model.dataBases.serialize.types.SerializableNode
 import bstrees.model.dataBases.serialize.types.SerializableTree
+import app.presenter.utils.TreeHeightPresenter
 
 object LayoutPresenter {
 
-    private val defaultNodeLength = 1
-    private val windowHeight = 800
-    private val windowWidth = 800
+    private const val nodeRadius = 50
+    private const val edgeLength = 50
 
-    fun setTreeLayout(tree: SerializableTree, height: Int, width: Int){
 
+    fun setTreeLayout(tree: SerializableTree, windowHeight: Int, windowWidth: Int){
+
+        val treeHeight = TreeHeightPresenter.getTreeHeight(tree)
+
+        tree.root?.let {root->
+            root.posX = 0
+            root.posY = 0
+
+            setNodesLayout(root, treeHeight)
+        }
     }
 
     // This method will assign coordinates to the nodes of the tree
-    private fun setNodesLayout(node:  SerializableNode){
-        node.posX = 0
-        node.posY = 0
+    private fun setNodesLayout(node:  SerializableNode, height: Int){
+        val xDiff = (edgeLength + 2 * nodeRadius) * height + (height - 1)
+        val yDiff = (edgeLength + 2 * nodeRadius)
+        node.leftNode?.let {leftNode->
+            leftNode.posX = node.posX - xDiff
+            leftNode.posY = node.posY + yDiff
 
-        node.leftNode?.let { setNodesLayout(it) }
-        node.rightNode?.let { setNodesLayout(it) }
+            setNodesLayout(leftNode, height - 1)
+        }
 
+        node.rightNode?.let { rightNode ->
+            rightNode.posX = node.posX + xDiff
+            rightNode.posY = node.posY + yDiff
+
+            setNodesLayout(rightNode, height - 1)
+        }
     }
 
 }
